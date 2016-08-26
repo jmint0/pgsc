36c733323020c2a800791d94bbb5c48d9397e313
jminto <jminto@BLADE>
2016-08-26 14:45:09 -0400

    Basic text read from file

--- src/pgsc/Main.java
+++ src/pgsc/Main.java
@@ -1,24 +1,50 @@
 package pgsc;
 
-import static org.bytedeco.javacpp.opencv_core.*;
-import static org.bytedeco.javacpp.opencv_imgproc.*;
-import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+import static org.bytedeco.javacpp.lept.pixDestroy;
+import static org.bytedeco.javacpp.lept.pixRead;
+
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.lept.PIX;
+import org.bytedeco.javacpp.tesseract.TessBaseAPI;
+
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String filename = "Ikea_Lady_Walking.jpg";
-		System.out.println(" ## Loading Image " + filename);
-        IplImage image = cvLoadImage(filename);
-        if (image != null) {
-        	System.out.println(" ## Smothing Image...");
-            cvSmooth(image, image);
-            System.out.println(" ## Saving Image...");
-            cvSaveImage(filename, image);
-            cvReleaseImage(image);
-        }
-        System.out.println(" ## Code Complete.");
-	}
+	public static void main(String[] args) throws Exception {
+		BytePointer outText;
+		
+		System.out.println(" ## System.getenv(\"TESSDATA_PREFIX\") = " + System.getenv("TESSDATA_PREFIX"));
+		
+		TessBaseAPI api = new TessBaseAPI();
+		// Initialize tesseract-ocr with English, without specifying tessdata path
+		if (api.Init(System.getenv("TESSDATA_PREFIX"), "ENG") != 0) {
+			System.err.print("Could not initialize tesseract.");
+			System.exit(1);
+		}
+		
+		// Open input image with leptonica library
+		PIX image = pixRead("C:\\Users\\jminto\\Pictures\\Pokemon\\Screenshot_2016-07-27-08-18-59.png");
+		if ( image == null ) {
+			System.out.println(" !! pixRead unable to open file");
+		} else {
+			System.out.println(" ## Image width: " + image.w());
+			System.out.println(" ## Image height: " + image.h());
+			api.SetImage(image);
+			// Get OCR result
+			outText = api.GetUTF8Text();
+			String string = outText.getString();
+			if ( string.isEmpty() ) 
+				System.out.println(" !! OCR did not detect string.");
+			else 
+				System.out.println("OCR output:\n" + string);
+			outText.deallocate();
+		} // good image
+		
+		// Destroy used object and release memory
+		api.End();
+
+		pixDestroy(image);
+		
+	} // main
 
 }
